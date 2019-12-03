a46d76536baf8969b440ca565fc6176cc06f813b
jimhendy <jimhendy88@gmail.com>
2019-12-03 05:58:00 +0000

    Day 3

---
+++ 2019/03/b.py
@@ -0,0 +1,24 @@
+import common
+import numpy as np
+
+def run(inputs):
+
+    wire_coords = common.get_wire_coords(inputs)
+    
+    intersections = common.get_intersections(
+        wire_coords
+    )
+
+    indices = np.array([
+        [
+            common.index_of_array(wire_coords[0], i),
+            common.index_of_array(wire_coords[1], i)
+        ]
+        for i in intersections
+    ])
+
+    min_steps = indices.sum(axis=1)
+    min_steps = [ i for i in min_steps if i != 0 ]
+    min_steps = min(min_steps)
+    
+    return min_steps
---
+++ 2019/03/common.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+
+def index_of_array(array, value):
+    return np.flatnonzero((array == value).all(1))[0]
+
+def get_intersections(wire_coords):
+    intersections = np.array(
+        [
+            x for x in
+            set(tuple(x) for x in wire_coords[0])
+            &
+            set(tuple(x) for x in wire_coords[1])
+        ]
+    )
+
+    return intersections
+
+def get_wire_coords(inputs):
+    wire_vertices = [w.split(',') for w in inputs.split(os.linesep)]
+    wire_coords = []
+    
+    for wire_num, wv in enumerate(wire_vertices):
+        previous_pos = np.array([0,0])
+        wire_coords.append([previous_pos])
+        for v in wv:
+            direction = v[0]
+            step = int(v[1:])
+            move_func = get_move_func(direction)
+            for s in range(step):
+                previous_pos = move_func(previous_pos)
+                wire_coords[wire_num].append(previous_pos)
+                pass
+            pass
+        pass
+    return wire_coords
+
+def get_move_func(direction):
+    step = {
+        'U':np.array([1,0]),
+        'D':np.array([-1,0]),
+        'R':np.array([0,1]),
+        'L':np.array([0,-1])
+    }.get(direction)
+    def f(p): return p + step
+    return f
+
