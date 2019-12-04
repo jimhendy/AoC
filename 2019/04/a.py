37c9b92a456d3d798bdf3a78310740d826b5e882
jimhendy <jimhendy88@gmail.com>
2019-12-04 19:26:39 +0000

    Finally finishing 4 b

--- 2019/04/a.py
+++ 2019/04/a.py
@@ -5,7 +5,10 @@ def run(inputs):
     diffs = common.diffs(possibles)
     counts = common.counts(possibles)
 
+    # Ensure all consecutive values increase
     diffs_mask = diffs.gt(-1).all(axis=1)
+    # Ensure at least one value is repeated
+    # As the values increase they must be beside each other
     counts_mask = counts.gt(1).any(axis=1)
     
     possibles = possibles[
--- 2019/04/b.py
+++ 2019/04/b.py
@@ -1,32 +1,18 @@
-import re
 import common
-import pandas as pd
 
 def run(inputs):
     possibles = common.possibles(inputs)
     diffs = common.diffs(possibles)
     counts = common.counts(possibles)
 
+    # Ensure all consecutive values increase
     diffs_mask = diffs.gt(-1).all(axis=1)
-    counts_mask = counts.gt(1).any(axis=1)
-    
-    diff_str = pd.Series(0, index=possibles.index)
-    for i in range(diffs.shape[1]-1):
-        col = diffs.astype(int).iloc[:,i+1]
-        diff_str += col.abs().mul(10**(diffs.shape[1]-2-i))
-        pass
-    diff_str = diff_str.astype(str)
-    
-    zeros_mask = (
-        diff_str.str.contains('[^0]0[^0]')
-        |
-        diff_str.str.contains('0[^0]$')
-        |
-        diff_str.str.contains('$0[^0]')
-    )
+    # Ensure one value is repeated only twice
+    # As all values increase this is enough
+    double_mask = counts.eq(2).any(axis=1)
     
     possibles = possibles[
-        diffs_mask & counts_mask & zeros_mask
+        diffs_mask & double_mask
     ]
     
     return len(possibles)
