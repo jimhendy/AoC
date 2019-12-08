9237282734e62ecfdc5dc20793e09a6d827dbeee
jimhendy <jimhendy88@gmail.com>
2019-12-08 06:13:36 +0000

    Day 8

---
+++ 2019/08/a.py
@@ -0,0 +1,19 @@
+import common
+import numpy as np
+
+def run(inputs):
+
+    data = common.in_to_array(inputs)
+    
+    # We don't care about row or column so resize to flatten that
+    data = data.reshape(data.shape[0],-1)
+
+    # Index of the fewest zeros
+    zeros = (data==0).astype(int).sum(axis=1)
+    index = zeros.argmin()
+
+    unique, counts = np.unique(data[index], return_counts=True)
+    counts = dict(zip(unique, counts))
+
+    return counts[1] * counts[2]
+    
---
+++ 2019/08/b.py
@@ -0,0 +1,26 @@
+import common
+import numpy as np
+import seaborn as sns
+import matplotlib.pylab as plt
+
+def run(inputs):
+
+    data = common.in_to_array(inputs)
+
+    # 2 means transparent so replace with nans
+    data = np.where( data==2, np.nan, data )
+
+    # Combine all the first visible layers
+    output = np.full( data.shape[1:], np.nan )
+    for layer in range(data.shape[0]):
+        output = np.where( np.isnan(output), data[layer], output)
+        pass
+
+    # Plot the result
+    sns.heatmap(output)
+    plt.show()
+    
+    result = input('What does it say: ')
+    
+    return result
+    
