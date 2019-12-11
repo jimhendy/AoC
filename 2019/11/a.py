688f2577f53b090795f7d361621c3e74a484db49
jimhendy <jimhendy88@gmail.com>
2019-12-11 20:00:04 +0000

    Day 11

--- 2019/11/a.py
+++ 2019/11/a.py
@@ -1,23 +1,11 @@
-import emergency_hull_painting_robot
+import emergency_hull_painting_robot as ehpr
 import matplotlib.pylab as plt
 import seaborn as sns
 import numpy as np
 
 def run(inputs):
 
-    robot = emergency_hull_painting_robot.EhpRobot(inputs)
+    robot = ehpr.EhpRobot(inputs)
     robot.run()
-
-    xy = np.array(
-        [
-            np.array([int(i) for i in k[1:-1].split()])
-            for k,v in robot._colors.items() if v == emergency_hull_painting_robot.Color.WHITE ]
-    ).T
-
-    ##sns.heatmap(xy)
-    #plt.show()
-    
-    import code
-    code.interact(local=locals())
     
     return len(robot._colors)
--- 2019/11/b.py
+++ 2019/11/b.py
@@ -1,25 +1,38 @@
-import emergency_hull_painting_robot
+import emergency_hull_painting_robot as ehpr
 import matplotlib.pylab as plt
 import seaborn as sns
+import pandas as pd
 import numpy as np
+
 
 def run(inputs):
 
-    robot = emergency_hull_painting_robot.EhpRobot(inputs, start_color=emergency_hull_painting_robot.Color.WHITE)
+    robot = ehpr.EhpRobot(inputs, start_color=ehpr.Color.WHITE)
     robot.run()
 
     xy = np.array(
         [
             np.array([int(i) for i in k[1:-1].split()])
-            for k,v in robot._colors.items() if v == emergency_hull_painting_robot.Color.WHITE ]
+            for k, v in robot._colors.items()
+            if v == ehpr.Color.WHITE
+        ]
     ).T
 
-    plt.scatter( xy[0], xy[1], s=50 )
+    df = pd.DataFrame({'x':xy[0], 'y':xy[1]})
+    df['Ones'] = 1
+
+    df = df.pivot_table(index='y', columns='x', values='Ones')    
+    for i in range(df.columns.max()):
+        if not i in df.columns:
+            df[i] = np.nan
+            
+    df = df[ sorted(df.columns) ]
+    df.sort_index(inplace=True, ascending=False)
+    
+    sns.heatmap(df.values, cbar=False)
+    plt.axis('off')
     plt.show()
 
-    import code
-    code.interact(local=locals())
-
     result = input('What does it say: ')
-    
+
     return result
