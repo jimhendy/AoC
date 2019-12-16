dafda8a079e7ed561792bdab21f2dd9de245f110
James Henderson <j.henderson@macbook003.wcm-london.com>
2019-12-16 18:37:21 +0000

    Day 16

--- 2019/16/b.py
+++ 2019/16/b.py
@@ -1,29 +1,34 @@
-import tqdm
 import numpy as np
+import numba
 
-BASE_PATTERN = [0,1,0,-1]
 
-def run_phase(inp, phase):
-    data = np.array([ int(i) for i in list(inp) ])
-    pattern_data = []
-    for i in tqdm.tqdm(range(data.shape[0])):
-        pat = np.repeat(BASE_PATTERN, i+1)
-        pat = np.tile( pat, int(np.ceil(data.shape[0]/pat.shape[0])) + 1 )[1:data.shape[0]+1]
-        pattern_data.append(np.mod(np.abs((data * pat).sum()),10))
+@numba.njit
+def run_phase(inp):
+    output = np.zeros(len(inp))
+    for i, x in enumerate(inp[::-1]):
+        output[-i-1] = (output[-i] + x) % 10
         pass
-    import code
-    code.interact(local=locals())
-    return ''.join([str(i) for i in ans]).zfill(data.shape[0])
+    return output
+
 
 def run(inputs):
 
-    phase = 0
-    inp = inputs * 10000
+    offset = int(inputs[:7])
+    data = list(inputs) * 10000
 
-    offset = inputs[:8]
-    
-    for phase in tqdm.tqdm(range(100)):
-        inp = run_phase(inp, phase)
+    # If this is the case then we only care about the cumulative sum
+    """
+    The top half of the matrix is complicated to compute but the lower right
+    is simply the cumsum of the values and "luckily" the offset means
+    we only care about the lower right corner
+    """
+    assert offset > len(data)/2
+
+    data = list(map(int, data[offset:]))
+
+    for i in range(100):
+        data = run_phase(data)
         pass
 
-    return inp[offset:8]
+    result = ''.join(map(str, map(int, data[:8])))
+    return result
