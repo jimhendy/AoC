747be29943368c8d4a575b6acf637a7e7991ce9d
jimhendy <jimhendy88@gmail.com>
2019-12-28 14:21:28 +0000

    Day 20

---
+++ 2019/21/b.py
@@ -0,0 +1,46 @@
+from intcode import Intcode
+import os
+
+
+def instruction(instr, prog):
+    [prog.analyse_intcode(ord(i)) for i in list(instr)]
+    prog.analyse_intcode(10)
+    pass
+
+
+def run(inputs):
+
+    prog = Intcode(inputs)
+
+    ins = [
+        # Either not B or not C
+        'NOT B J',
+        'NOT C T',
+        'OR J T',
+        # And D
+        'AND D T',
+        # Reset J to True
+        'NOT T J',
+        'OR T J',
+        # E or H (next landing site)
+        'AND E J',
+        'OR H J',
+        # All above
+        'AND J T',
+        # Need to jump if not A
+        'NOT A J',
+        'OR T J',
+        'RUN'
+    ]
+
+    [instruction(i, prog) for i in ins]
+    [
+        print(i)
+        for i in ''.join(
+                [
+                    chr(i)
+                    for i in prog.outputs[:-1]
+                ]
+        ).split(os.linesep)
+    ]
+    return prog.outputs[-1]
