56204e0a648ea0ff097ed166b9ff0e649d84e54e
jimhendy <jimhendy88@gmail.com>
2019-12-28 15:58:58 +0000

    Day 22

--- 2019/22/a.py
+++ 2019/22/a.py
@@ -1,8 +1,26 @@
 import os
+import numba
+import numpy as np
 
+@numba.njit(numba.int64[:](numba.int64[:]))
+def deal_into_new_stack(cards):
+    return cards[::-1]
+
+@numba.njit(numba.int64[:](numba.int64[:], numba.int64))
+def deal_with_increment(cards, inc):
+    out = cards.copy()
+    j = 0
+    for i in range(cards.shape[0]):
+        out[j] = cards[i]
+        j = ( j + inc ) % cards.shape[0]
+        pass
+    return out
+
+def cut(cards, N):
+    return np.roll(cards, -N)
+    
 def run(inputs):
-
-    cards = list( range(10) )
+    cards = np.array( range(10007) )
 
     instructions = []
     for i in inputs.split(os.linesep):
@@ -13,5 +31,13 @@ def run(inputs):
         instructions.append(ins)
         pass
     
-    import code
-    code.interact(local=locals())
+    for i in instructions:
+        if type(i) == tuple:
+            cards = globals()[i[0]]( cards, *i[1:] )
+            pass
+        else:
+            cards = globals()[i]( cards )
+            pass
+        pass
+
+    return np.argwhere( cards == 2019 )
