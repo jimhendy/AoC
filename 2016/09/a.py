3b3027085580d0595821089b26b0045d5924f995
jimhendy <jimhendy88@gmail.com>
2020-03-15 16:21:59 +0000

    2016 Day 9

---
+++ 2016/09/a.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+
+def run(inputs):
+
+    data = ''.join(inputs.split(os.linesep))
+
+    marker_reg = re.compile('\((\d+)x(\d+)\)')
+    prev_end_pos = 0
+
+    output = ''
+
+    for match in marker_reg.finditer(data):
+
+        start, end = match.span()
+        if start < prev_end_pos:
+            # Still inside the previous marker
+            continue
+
+        # fill anything missing
+        output += data[prev_end_pos:start]
+
+        n_chars, repeats = list(map(int, match.groups()))
+        prev_end_pos = end + n_chars
+        chars = data[end:prev_end_pos]
+        output += chars * repeats
+
+    output += data[prev_end_pos:]
+    return len(output)
---
+++ 2016/09/b.py
@@ -0,0 +1,22 @@
+import os
+import re
+import numpy as np
+
+
+def run(inputs):
+
+    data = ''.join(inputs.split(os.linesep))
+    marker_reg = re.compile('\((\d+)x(\d+)\)')
+
+    multiples = np.ones(len(data))
+    for match in marker_reg.finditer(data):
+
+        start, end = match.span()
+        multiples[start:end] = 0
+
+        n_chars, repeats = list(map(int, match.groups()))
+        multiples[end:end+n_chars] *= repeats
+
+        pass
+
+    return multiples.sum()
