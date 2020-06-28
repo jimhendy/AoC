26c434f9d7c1ec6156bfdff1efe37544b40ab38d
jimhendy <jimhendy@hotmail.com>
2020-06-28 14:12:23 +0100

    Day 24

---
+++ 2016/24/a.py
@@ -0,0 +1,46 @@
+import maze
+import a_star
+
+
+class MazeState(a_star.State):
+    def __init__(
+        self, maze, current_number, previous_steps=0, collected_numbers=None
+    ):
+        self.maze = maze
+        self.current_number = current_number
+        self.collected_numbers = [] if collected_numbers is None else collected_numbers
+        self.collected_numbers += [current_number]
+        self.previous_steps = previous_steps
+        super().__init__()
+
+    def is_valid(self):
+        return True
+
+    def is_complete(self):
+        return len(self.collected_numbers) == len(self.maze.number_locations())
+
+    def __lt__(self, other):
+        return self.previous_steps < other.previous_steps
+
+    def all_possible_next_states(self):
+        for next_number in self.maze.number_locations().keys():
+            if next_number in self.collected_numbers:
+                continue
+            yield MazeState(
+                self.maze,
+                next_number,
+                self.previous_steps + self.maze.steps_between(self.current_number, next_number),
+                self.collected_numbers[:]
+            )
+
+
+def run(inputs):
+    m = maze.Maze(inputs)
+    initial_state = MazeState(m, 0)
+    best_route = a_star.a_star(
+        initial_state, 
+        tag_func=lambda x : str(x.collected_numbers)
+    )
+
+    return best_route.previous_steps
+
---
+++ 2016/24/maze.py
@@ -0,0 +1,103 @@
+import os
+import re
+from functools import lru_cache
+
+import numpy as np
+
+import a_star
+
+WALL_CHAR = "#"
+SPACE_CHAR = "."
+
+
+class Maze:
+    def __init__(self, maze_as_str):
+        self.maze_as_str = maze_as_str
+        self.maze = np.array(
+            [
+                [j for j in i] 
+                for i in 
+                self.maze_as_str.split(os.linesep)
+            ]
+        )
+        self.steps_between_store = {}
+
+    @lru_cache(maxsize=1)
+    def number_locations(self):
+        return {
+            int(n): np.array((x, y))
+            for x, row in enumerate(self.maze)
+            for y, n in enumerate(row)
+            if Maze.char_is_number(n)
+        }
+
+    @staticmethod
+    @lru_cache(maxsize=48)
+    def char_is_number(char):
+        return re.search(r"^-?\d+$", char)
+
+    @staticmethod
+    def store_key(num_a, num_b):
+        sorted_nums = sorted([num_a, num_b])
+        return f"{sorted_nums[0]}_{sorted_nums[1]}"
+
+    def steps_between(self, num_start, num_end):
+        key = Maze.store_key(num_start, num_end)
+        if key not in self.steps_between_store.keys():
+            pos_start = self.number_locations()[num_start]
+            pos_end = self.number_locations()[num_end]
+            distance = self.find_distance(pos_start, pos_end)
+            self.steps_between_store[key] = distance
+        return self.steps_between_store[key]
+
+    def find_distance(self, pos_a, pos_b):
+        start_state = Position(self.maze, pos_a, pos_b)
+        end_state = a_star.a_star(
+            start_state,
+            tag_func = lambda x : str(x.current_pos) 
+        )
+        return end_state.previous_steps
+
+def get_char(maze, pos):
+    return maze[pos[0]][pos[1]]
+
+class Position(a_star.State):
+    def __init__(self, maze, current_pos, end_pos, previous_steps=0):
+        self.maze = maze
+        self.end_pos = end_pos
+        self.current_pos = current_pos
+        self.previous_steps = previous_steps
+        super().__init__()
+
+    def is_valid(self):
+        if np.any(self.current_pos < 0):
+            #print(f'Failed {self.current_pos} as negative')
+            return False
+        if np.any(self.current_pos >= self.maze.shape):
+            #print(f'Failed {self.current_pos} as larger than maze')
+            return False
+        current_char = get_char(self.maze, self.current_pos)
+        if current_char == WALL_CHAR:
+            #print(f'Failed {self.current_pos} as wall')
+            return False
+        return True
+
+    def is_complete(self):
+        return np.all(self.current_pos == self.end_pos)
+
+    def all_possible_next_states(self):
+        #print(f'Current : {self.current_pos}')
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if (dx and dy) or (dx == dy == 0):
+                    continue
+                #print(f'New {self.current_pos + np.array((dx, dy))}')
+                yield Position(
+                    self.maze,
+                    self.current_pos + np.array((dx, dy)),
+                    self.end_pos,
+                    self.previous_steps + 1,
+                )
+
+    def __lt__(self, other):
+        return self.previous_steps < other.previous_steps
