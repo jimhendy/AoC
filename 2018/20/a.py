ebfbfe081f351c734bd98c88cd2e9c22849a5d4a
jimhendy <jimhendy88@gmail.com>
2020-12-11 07:31:15 +0000

    2018 Day 20 Start

--- 2018/20/a.py
+++ 2018/20/a.py
@@ -1,63 +1,51 @@
-import networkx as nx
-import re
-
 def extract_sub_options(txt):
+
+    if not '(' in txt:
+        return txt.split('|')
+
     output = ['']
-    level = 0
-    for c in txt:
-        if c == '(':
+    level  = 0
+
+    bracket_start, bracket_end = None, None
+
+    for i, ci in enumerate(txt):
+        
+        if ci == '(':
             level += 1
+            if level == 1:
+                bracket_start = i
+        
 
         if not level:
             for i,o in enumerate(output):
-                output[i] = o + c
-        elif 
-
-
-
-
-        if level == ')':
-            level -= 1
-
-
-
-'''
-
-def extract_sub_options(txt, base=""):
-    if len(txt) and txt[0] == '(':
-        print(txt)
-        txt = txt.lstrip('(').rstrip(')')
-    if not '|' in txt:
-        return [ base + txt ]
-    print(txt)
-    output = []
-    level = 0
-    this_output = ""
-    for c in txt:
-
-        if c == "(":
-            level += 1
-        elif c == ")":
-            level -= 1
+                output[i] = o + ci
         
-        if not level:
-            if c == "|":
-                print(f'- {this_output}')
-                output.extend(extract_sub_options(this_output, base))
-                this_output = ""
-            elif c not in ['(',')']:
-                base += c
-        else:
-            this_output += c
-
-    if len(this_output):
-        print(f'Final = {this_output}')
-        output.extend(extract_sub_options(this_output, base))
+        if ci == ')':
+            level -= 1
+            if level == 0:
+                bracket_end = i
+    
+        if bracket_start is not None and bracket_end is not None and not level:
+            sub_options = extract_sub_options(
+                txt[bracket_start+1:bracket_end]
+            )
+            print(sub_options)
+            for i,o in enumerate(output):
+                for j, so in enumerate(sub_options):
+                    if not j:
+                        output[i] = o + so
+                    else:
+                        output.append(o+so)
+            bracket_start = None
+            bracket_end = None
+    
     return output
-'''
+
+
 
 def run(inputs):
-    inputs = "^ENWWW(NEEE|SSE(EE|N))$"
+    inputs = "^E(N|S(E|W))N$"
+    inputs = '^N(E|W)$'
     inputs = inputs.strip().rstrip("$").lstrip("^")
 
     print(extract_sub_options(inputs))
