c73b74ff39936896ceff9d60c2611f73e3e4c291
jimhendy <jimhendy88@gmail.com>
2020-12-19 08:02:15 +0000

    2020 Day 19

---
+++ 2020/19/a.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+
+def run(inputs):
+    rules, messages = inputs.split(os.linesep * 2)
+    rules = {
+        r.split(":")[0]: "( " + r.split(":")[1].replace('"', "") + " )"
+        for r in rules.split(os.linesep)
+    }
+    messages = messages.split(os.linesep)
+
+    while any(
+        [character.isdigit() for rule_reg in rules.values() for character in rule_reg]
+    ):
+        for rule_num, rule_reg in rules.items():
+            for sub_rule_num in re.findall(r"(\d+)", rule_reg):
+                rules[rule_num] = re.sub(
+                    f" {sub_rule_num} ",
+                    " " + rules[sub_rule_num] + " ",
+                    rules[rule_num],
+                )
+
+    for k, v in rules.items():
+        rules[k] = "^" + v.replace(" ", "") + "$"
+
+    total = 0
+    for m in messages:
+        if re.search(rules["0"], m):
+            total += 1
+
+    return total
---
+++ 2020/19/b.py
@@ -0,0 +1,66 @@
+import os
+import re
+
+
+def run(inputs):
+    rules, messages = inputs.split(os.linesep * 2)
+    rules = {
+        r.split(":")[0]: "(?: " + r.split(":")[1].replace('"', "") + " )"
+        for r in rules.split(os.linesep)
+    }
+    messages = messages.split(os.linesep)
+
+    while any(
+        [character.isdigit() for rule_reg in rules.values() for character in rule_reg]
+    ):
+        for rule_num, rule_reg in rules.items():
+            for sub_rule_num in re.findall(r"(\d+)", rule_reg):
+                rules[rule_num] = re.sub(
+                    f" {sub_rule_num} ",
+                    " " + rules[sub_rule_num] + " ",
+                    rules[rule_num],
+                )
+
+    for k, v in rules.items():
+        rules[k] = v.replace(" ", "")
+
+    # rules['8'] = '( 42 | 42 8 )'
+    # rules['11'] = '( 42 31 | 42 11 31 )'
+
+    # 0 = 8 11
+    # Match 42 31 from centre
+    # Any number of 42s on the left
+
+    reg = re.compile(
+        "^(" + rules["42"] + ")(" + rules["42"] + ")+(" + rules["31"] + ")+$"
+    )
+    reg_11_rep = re.compile(
+        "^(?:"
+        + rules["42"]
+        + ")+"
+        + "("
+        + rules["42"]
+        + rules["31"]
+        + ")(?:"
+        + rules["31"]
+        + ")*$"
+    )
+    reg_8 = re.compile("^(" + rules["42"] + ")+$")
+
+    total = 0
+    for m in messages:
+        if not reg.search(m):
+            # Test if this line roughly matches the desired pattern
+            continue
+        while reg_11_rep.match(m):
+            # Remove all inner couples of 42 + 31
+            # match indicies refer to input m so as we are altering m in each iteration must retest every time (I think)
+            for match in reg_11_rep.finditer(m):
+                m = m[: match.start(1)] + m[match.end(1) :]
+                break
+        if not reg_8.search(m):
+            # Ensure we still have at least one 42 at the start
+            continue
+        total += 1
+
+    return total
