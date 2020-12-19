048c10c8d9e154ffb48555792172d830f5a84fa1
jimhendy <jimhendy88@gmail.com>
2020-12-19 08:11:12 +0000

    Documentation for wee Balum

--- 2020/06/a.py
+++ 2020/06/a.py
@@ -3,15 +3,19 @@ import os
 def run(inputs):
 
     total = 0
-    data = set()
+    data = set() # Use a set to store the answers from each group
     for line in inputs.split(os.linesep):
         if not len(line.strip()):
+            # Reaching an empty line means a new group so add the current total and reset
             total += len(data)
             data = set()
         else:
             for letter in list(line.strip()):
+                # Add each letter to the current group's answers
+                # As data is a set we don't have to worry about duplicates
                 data.add(letter)
 
+    # We may still have some un-counted results for the final group if the input did not end on a blank line, otherwise we will add zero below
     total += len(data)
 
     return total
--- 2020/06/b.py
+++ 2020/06/b.py
@@ -4,20 +4,24 @@ import os
 def run(inputs):
 
     total = 0
-    data = None
+    data = None  # We will use a set to store the letters again but need a special way of knowing when we are starting a new group. Note: len(data) == 0 is not valid as then we can't tell between a new group and an existing group with no overlapping answers
     for line in inputs.split(os.linesep):
 
         line = line.strip()
 
         if not len(line):
+            # Reaching an empty line means a new group so add the current total and reset
             total += len(data)
             data = None
         else:
             if data is None:
+                # The first person in the group, add all their answers to data and make it a set
                 data = set(list(line))
             else:
+                # Another person in the same group, add their letters to the set only if they are already present
                 data = data.intersection(set(list(line)))
 
+    # We may still have some un-counted results for the final group if the input did not end on a blank line, otherwise we will add zero below
     total += len(data)
 
     return total
