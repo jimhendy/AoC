f7d7ac96d6db501dcfd7b3fda4a59fd9af0cb479
jimhendy <jimhendy88@gmail.com>
2021-12-09 06:12:30 +0000

    2021 Day 9

--- 2021/09/a.py
+++ 2021/09/a.py
@@ -1,8 +1,28 @@
 import os
-import re
 import numpy as np
-import pandas as pd
-from tools import *
+from tools.point import Point2D
+
 
 def run(inputs):
-    pass
+    total_risk = 0
+    heightmap = np.array([list(l) for l in inputs.split(os.linesep)]).astype(int)
+    for x in range(heightmap.shape[1]):
+        for y in range(heightmap.shape[0]):
+            h = heightmap[y, x]
+            p = Point2D(x, y)
+            is_lowest = True
+            for pp in p.nb4():
+
+                if pp.x[0] < 0 or pp.x[0] == heightmap.shape[1]:
+                    continue
+                if pp.x[1] < 0 or pp.x[1] == heightmap.shape[0]:
+                    continue
+
+                if heightmap[pp.x[1], pp.x[0]] <= h:
+                    is_lowest = False
+                    break
+
+            if is_lowest:
+                total_risk += h + 1
+
+    return total_risk
--- 2021/09/b.py
+++ 2021/09/b.py
@@ -1,8 +1,51 @@
 import os
-import re
 import numpy as np
-import pandas as pd
-from tools import *
+from tools.point import Point2D
+
 
 def run(inputs):
-    pass
+
+    heightmap = np.array([list(l) for l in inputs.split(os.linesep)]).astype(int)
+    basin_sizes = []
+    for x in range(heightmap.shape[1]):
+        for y in range(heightmap.shape[0]):
+            h = heightmap[y, x]
+            low_point = Point2D(x, y)
+            is_lowest = True
+            for pp in low_point.nb4():
+
+                if pp.x[0] < 0 or pp.x[0] == heightmap.shape[1]:
+                    continue
+                if pp.x[1] < 0 or pp.x[1] == heightmap.shape[0]:
+                    continue
+
+                if heightmap[pp.x[1], pp.x[0]] <= h:
+                    is_lowest = False
+                    break
+
+            if is_lowest:
+                points_in_basin = set([low_point])
+                points_to_analyse = [low_point]
+                seen = set([low_point])
+                while len(points_to_analyse):
+                    p = points_to_analyse.pop()
+
+                    for pp in p.nb4():
+
+                        if pp in seen:
+                            continue
+                        if pp.x[0] < 0 or pp.x[0] == heightmap.shape[1]:
+                            continue
+                        if pp.x[1] < 0 or pp.x[1] == heightmap.shape[0]:
+                            continue
+
+                        if heightmap[pp.x[1], pp.x[0]] == 9:
+                            continue
+
+                        seen.add(pp)
+                        points_in_basin.add(pp)
+                        points_to_analyse.append(pp)
+
+                basin_sizes.append(len(points_in_basin))
+
+    return np.prod(sorted(basin_sizes)[-3:])
