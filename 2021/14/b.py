88c3dec1201e141d2e28e248f4ea67eababe39f5
jimhendy <jimhendy88@gmail.com>
2021-12-14 16:53:54 +0000

    2021 Day 14

--- 2021/14/b.py
+++ 2021/14/b.py
@@ -1,8 +1,29 @@
 import os
-import re
-import numpy as np
-import pandas as pd
-from tools import *
+from collections import defaultdict
+
 
 def run(inputs):
-    pass
+
+    initial, _, *alteration_inputs = inputs.split(os.linesep)
+    alterations = {k: v for k, v in (a.split(" -> ") for a in alteration_inputs)}
+
+    pairs = defaultdict(int)
+    for i, j in zip(initial[:-1], initial[1:]):
+        pairs[i + j] += 1
+
+    for _ in range(40):
+        new_pairs = defaultdict(int)
+        for old_pair, num in pairs.items():
+            to_inset = alterations[old_pair]
+            new_pairs[old_pair[0] + to_inset] += num
+            new_pairs[to_inset + old_pair[1]] += num
+        pairs = new_pairs
+
+    counts = defaultdict(int)
+    for pair, num in pairs.items():
+        counts[pair[0]] += num
+        counts[pair[1]] += num
+
+    count_values = list(counts.values())
+
+    return (max(count_values) - min(count_values)) // 2 + 1
--- p.py
+++ p.py
@@ -1,30 +1,42 @@
 import os
 from collections import deque, Counter
 
-
 def step(q, alterations):
     len_before_step = len(q)
     for _ in range(len_before_step - 1):
-
+        
         starting_pair = q[0] + q[1]
         to_insert = alterations.get(starting_pair)
 
         if to_insert:
+            #print(f'Inserting {to_insert} between {starting_pair}')
             q.rotate(-1)
             q.appendleft(to_insert)
 
         q.rotate(-1)
     q.rotate(-1)
 
-
 def run(inputs):
-    initial, _, *alteration_inputs = inputs.split(os.linesep)
-    alterations = {k: v for k, v in (a.split(" -> ") for a in alteration_inputs)}
+    initial, _, *alt_inputs = inputs.split(os.linesep)
     q = deque(list(initial))
-
-    [step(q, alterations) for _ in range(10)]
+    alterations = {}
+    for a in alt_inputs:
+        a_split = a.split(' -> ')
+        if len(a_split) != 2:
+            continue
+        alterations[a_split[0]] = a_split[1]
+    
+    for i in range(40):
+        print(i)
+        step(q, alterations)
 
     counts = Counter(q)
     count_values = list(counts.values())
 
     return max(count_values) - min(count_values)
+
+if __name__ == '__main__':
+    with open('test.txt', 'r') as f:
+        inputs = f.read()
+    print(inputs)
+    print(run(inputs))
