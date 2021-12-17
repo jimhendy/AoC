222d08aa5dbb38eedec405930af4ed093b78a6e6
jimhendy <jimhendy88@gmail.com>
2021-12-17 13:28:49 +0000

    Understood the maths

--- 2021/17/a.py
+++ 2021/17/a.py
@@ -2,36 +2,5 @@ import numpy as np
 
 
 def run(inputs):
-    x_target = list(map(int, inputs.split("x=")[1].split(",")[0].split("..")))
-    y_target = list(map(int, inputs.split("y=")[1].split("..")))
-
-    xs = np.arange(x_target[1] + 1)
-    ys = np.arange(y_target[0], max(np.abs(y_target)) + 1)
-
-    velocities = np.array(np.meshgrid(xs, ys)).T.reshape(-1, 2)
-
-    pos = np.full(velocities.shape, 0)
-    below_target = np.zeros(pos.shape[0]).astype(bool)
-    max_height = np.zeros_like(below_target)
-    hit_target = np.zeros_like(below_target).astype(bool)
-
-    while False in below_target:
-        pos += velocities
-
-        velocities[velocities[:, 0] > 0, 0] -= 1
-        velocities[velocities[:, 0] < 0, 0] += 1
-        velocities[:, 1] -= 1
-
-        below_target[pos[:, 1] < y_target[1]] = True
-
-        height = pos[:, 1]
-        max_height = np.maximum(max_height, height)
-
-        in_target = np.logical_and(
-            np.logical_and(x_target[0] <= pos[:, 0], pos[:, 0] <= x_target[1]),
-            np.logical_and(y_target[0] <= pos[:, 1], pos[:, 1] <= y_target[1]),
-        )
-        hit_target[in_target] = True
-
-    good_heights = max_height[hit_target]
-    return good_heights.max()
+    y_lower = np.abs(-int(inputs.split("y=")[1].split("..")[0]))
+    return y_lower * (y_lower-1) // 2
