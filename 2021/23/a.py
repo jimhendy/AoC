ab616a642a78b7c5cacc319ff567356e64185b73
jimhendy <jimhendy88@gmail.com>
2021-12-23 22:51:47 +0000

    Checking in for further work

--- 2021/23/a.py
+++ 2021/23/a.py
@@ -2,8 +2,356 @@ import os
 import re
 import numpy as np
 import pandas as pd
-from tools import *
+from tools.a_star import State, a_star
+from typing import List, Union, Dict, Tuple
+
+
+class Amphipod:
+    def __init__(
+        self,
+        type: str,
+        id_number: int,
+        hallway_pos: Union[None, int] = None,
+        entered_room: bool = False,
+    ):
+        self.type = type
+        self.cost = {"A": 1, "B": 10, "C": 100, "D": 1_000}[type]
+        self.hallway_pos = hallway_pos
+        self.entered_room = entered_room
+        self.id_number = id_number
+
+    def copy(self) -> "Amphipod":
+        return Amphipod(
+            type=self.type,
+            hallway_pos=self.hallway_pos,
+            entered_room=self.entered_room,
+            id_number=self.id_number,
+        )
+
+    def __repr__(self):
+        return f"{self.type}, {self.cost}, {self.hallway_pos}, {self.entered_room}, {self.id_number}"
+
+
+class Hallway:
+    def __init__(self, length: int, occupied: Dict[int, int] = None):
+        self.length = length
+        self.occupied = (
+            {}
+            if occupied is None
+            else occupied  # hallway position(int, zero based) to Amphipod id_number
+        )
+
+    def move_amphipod(self, old: int, new: int, amphipod_id: int) -> "Hallway":
+        assert new not in self.occupied
+        new_occupied = {k: v for k, v in self.occupied.items() if k != old}
+        new_occupied[new] = amphipod_id
+        return Hallway(length=self.length, occupied=new_occupied)
+
+    def copy(self) -> "Hallway":
+        return Hallway(
+            length=self.length, occupied={k: v for k, v in self.occupied.items()}
+        )
+
+
+class Room:
+    def __init__(
+        self,
+        top_amphipod_id: Union[None, int],
+        bottom_amphipod_id: Union[None, int],
+        hallway_pos: int,
+        desired_type: str,
+    ):
+        self.top_amphipod_id = top_amphipod_id
+        self.bottom_amphipod_id = bottom_amphipod_id
+        self.hallway_pos = hallway_pos
+        self.desired_type = desired_type
+
+    def can_add(self, amphipod: Amphipod, all_amphipods: List[Amphipod]) -> bool:
+        if amphipod.type != self.desired_type:
+            return False
+        if self.bottom_amphipod_id is None:
+            return True
+        if self.top_amphipod_id is not None:
+            return False
+        bottom = [a for a in all_amphipods if a.id_number == self.bottom_amphipod_id][0]
+        return amphipod.type == bottom.type
+
+    def add_amphipod(self, amphipod_id: int) -> "Room":
+        if self.bottom_amphipod_id is None:
+            return Room(
+                top_amphipod_id=None,
+                bottom_amphipod_id=amphipod_id,
+                hallway_pos=self.hallway_pos,
+                desired_type=self.desired_type,
+            )
+        elif self.top_amphipod_id is None:
+            return Room(
+                top_amphipod_id=amphipod_id,
+                bottom_amphipod_id=self.bottom_amphipod_id,
+                hallway_pos=self.hallway_pos,
+                desired_type=self.desired_type,
+            )
+        else:
+            raise RuntimeError(f"No space to add a new amphipod")
+
+    def __repr__(self):
+        return f"{self.top_amphipod_id}_{self.bottom_amphipod_id}"
+
+    def get_next_amphipod_id(self) -> Union[int, None]:
+        if self.top_amphipod_id is not None:
+            return self.top_amphipod_id
+        elif self.bottom_amphipod_id is not None:
+            return self.bottom_amphipod_id
+        else:
+            return None
+
+    def copy(self) -> "Room":
+        return Room(
+            top_amphipod_id=self.top_amphipod_id,
+            bottom_amphipod_id=self.bottom_amphipod_id,
+            hallway_pos=self.hallway_pos,
+            desired_type=self.desired_type,
+        )
+
+    def is_complete(self, all_amphipods: List[Amphipod]) -> bool:
+        if self.top_amphipod_id is None or self.bottom_amphipod_id is None:
+            return False
+        top_type = [a for a in all_amphipods if a.id_number == self.top_amphipod_id][
+            0
+        ].type
+        bottom_type = [
+            a for a in all_amphipods if a.id_number == self.bottom_amphipod_id
+        ][0].type
+        return top_type == bottom_type == self.desired_type
+
+
+class Route(State):
+    def __init__(
+        self,
+        hallway: Hallway,
+        rooms: List[Room],
+        amphipods: List[Amphipod],
+        energy: int = 0,
+        history: Union[None, List[Tuple[int, str]]] = None,
+    ):
+        self.hallway = hallway
+
+        self.rooms = rooms
+        self.amphipods = amphipods
+        self.energy = energy
+        self.history = history if history is not None else []
+        self.history.append((self.energy, self.__repr__()))
+
+    def is_complete(self):
+        return all([r.is_complete(all_amphipods=self.amphipods) for r in self.rooms])
+
+    def is_valid(self):
+        return True
+
+    def __lt__(self, other):
+        return self.energy < other.energy
+
+    def _amphipod_from_id(self, id_number: int) -> str:
+        return [a for a in self.amphipods if a.id_number == id_number][0]
+
+    def __repr__(self):
+        s = "#" * (self.hallway.length + 2) + os.linesep + "#"
+        for hallway_pos in range(self.hallway.length):
+            if hallway_pos in self.hallway.occupied:
+                s += self._amphipod_from_id(self.hallway.occupied[hallway_pos]).type
+            else:
+                s += "."
+        s += "#" + os.linesep + "#"
+        for pos in range(self.hallway.length):
+            found = False
+            for r in self.rooms:
+                if r.hallway_pos == pos:
+                    top = r.top_amphipod_id
+                    if top is None:
+                        s += " "
+                    else:
+                        s += self._amphipod_from_id(top).type
+                    found = True
+                    break
+            if not found:
+                s += " "
+        s += "#" + os.linesep + "#"
+        for pos in range(self.hallway.length):
+            found = False
+            for r in self.rooms:
+                if r.hallway_pos == pos:
+                    bottom = r.bottom_amphipod_id
+                    if bottom is None:
+                        s += " "
+                    else:
+                        s += self._amphipod_from_id(bottom).type
+                    found = True
+                    break
+            if not found:
+                s += " "
+        s += "#"
+        return s
+
+    def all_possible_next_states(self):
+        # Hallway chaps can move to other hallway spots or into empty rooms (can't pass each other)
+        occupied = sorted(list(self.hallway.occupied.keys()))
+        for hallway_pos, amphipod_id in self.hallway.occupied.items():
+            amphipod = [a for a in self.amphipods if a.id_number == amphipod_id][0]
+            if amphipod.entered_room:
+                raise RuntimeError(
+                    f"Shouldn't have dudes in the hallway that have already entered a room"
+                )
+
+            for new_pos in range(self.hallway.length):
+                # if any([new_pos == r.hallway_pos for r in self.rooms]):
+                #    continue
+                if new_pos == hallway_pos or new_pos in occupied:
+                    continue
+                elif new_pos < hallway_pos:
+                    crossing_other = any([new_pos < i < hallway_pos for i in occupied])
+                else:
+                    crossing_other = any([new_pos > i > hallway_pos for i in occupied])
+                if crossing_other:
+                    continue
+
+                outside_room = [r for r in self.rooms if r.hallway_pos == new_pos]
+                if len(outside_room):
+                    assert len(outside_room) == 1
+                    r = outside_room[0]
+                    if not r.can_add(amphipod=amphipod, all_amphipods=self.amphipods):
+                        continue
+                    new_room = r.add_amphipod(amphipod_id=amphipod.id_number)
+                    new_amphipod = amphipod.copy()
+                    new_amphipod.hallway_pos = None
+                    new_amphipod.entered_room = True
+
+                    new_rooms = [rr.copy() for rr in self.rooms if r is not rr]
+                    new_rooms.append(new_room)
+
+                    new_amphipods = [a.copy() for a in self.amphipods if a != amphipod]
+                    new_amphipods.append(new_amphipod)
+
+                    new_hallway = self.hallway.copy()
+                    del new_hallway.occupied[hallway_pos]
+
+                    is_bottom = new_room.top_amphipod_id is None
+                    extra_energy = amphipod.cost if is_bottom else 0
+
+                    yield Route(
+                        hallway=new_hallway,
+                        rooms=new_rooms,
+                        amphipods=new_amphipods,
+                        energy=self.energy
+                        + np.abs(new_pos - hallway_pos) * amphipod.cost
+                        + amphipod.cost
+                        + extra_energy,
+                        history=self.history[:],
+                    )
+                else:
+                    new_hallway = self.hallway.move_amphipod(
+                        old=hallway_pos, new=new_pos, amphipod_id=amphipod.id_number
+                    )
+                    new_rooms = [r.copy() for r in self.rooms]
+                    new_amphipods = [a.copy() for a in self.amphipods if a != amphipod]
+                    new_moved_amphiod = amphipod.copy()
+                    new_moved_amphiod.hallway_pos = new_pos
+                    new_amphipods.append(new_moved_amphiod)
+
+                    yield Route(
+                        hallway=new_hallway,
+                        rooms=new_rooms,
+                        amphipods=new_amphipods,
+                        energy=self.energy
+                        + amphipod.cost * np.abs(new_pos - hallway_pos),
+                        history=self.history[:],
+                    )
+
+        # Room chaps can move out only if the have not already done so
+        for r in self.rooms:
+            next_amphipod_id = r.get_next_amphipod_id()
+            if next_amphipod_id is None:
+                continue
+            next_amphipod = [
+                a for a in self.amphipods if a.id_number == next_amphipod_id
+            ][0]
+            if next_amphipod.entered_room:
+                continue
+            for new_pos in range(self.hallway.length):
+                if new_pos in occupied:
+                    continue
+                elif any([rr.hallway_pos == new_pos for rr in self.rooms]):
+                    continue
+                elif new_pos < r.hallway_pos:
+                    crossing_other = any(
+                        [new_pos < i < r.hallway_pos for i in occupied]
+                    )
+                else:
+                    crossing_other = any(
+                        [new_pos > i > r.hallway_pos for i in occupied]
+                    )
+                if crossing_other:
+                    continue
+
+                new_rooms = [rr.copy() for rr in self.rooms if rr is not r]
+                new_room = r.copy()
+                if new_room.top_amphipod_id is not None:
+                    new_room.top_amphipod_id = None
+                    extra_energy = 0
+                else:
+                    new_room.bottom_amphipod_id = None
+                    extra_energy = next_amphipod.cost
+                new_rooms.append(new_room)
+
+                new_hallway = self.hallway.copy()
+                new_hallway.occupied[new_pos] = next_amphipod_id
+
+                new_amphipods = [a.copy() for a in self.amphipods if a != next_amphipod]
+                new_amphipod = next_amphipod.copy()
+                new_amphipod.hallway_pos = new_pos
+                new_amphipods.append(new_amphipod)
+
+                yield Route(
+                    hallway=new_hallway,
+                    rooms=new_rooms,
+                    amphipods=new_amphipods,
+                    energy=self.energy
+                    + np.abs(new_pos - r.hallway_pos) * next_amphipod.cost
+                    + next_amphipod.cost
+                    + extra_energy,
+                    history=self.history[:],
+                )
 
 
 def run(inputs):
-    pass
+    lines = inputs.split(os.linesep)
+    hallway = Hallway(length=lines[1].count("."))
+
+    top_amphipods = lines[2].replace("#", "").strip()
+    bottom_amphipods = lines[3].replace("#", "").strip()
+
+    amphipods = [
+        Amphipod(type=t, id_number=i)
+        for i, t in enumerate(list(top_amphipods) + list(bottom_amphipods))
+    ]
+    print([a for a in amphipods])
+
+    rooms = [
+        Room(
+            top_amphipod_id=i,
+            bottom_amphipod_id=i + len(top_amphipods),
+            hallway_pos=h,
+            desired_type=t,
+        )
+        for i, (h, t) in enumerate([(2, "A"), (4, "B"), (6, "C"), (8, "D")])
+    ]
+
+    initial_state = Route(hallway=hallway, rooms=rooms, amphipods=amphipods)
+
+    best_route = a_star(initial_state=initial_state, tag_func=lambda x: x.__repr__())
+
+    print(best_route.energy)
+    for h in best_route.history:
+        print(h[1])
+    print()
+
+    return best_route.energy
--- 2021/23/b.py
+++ 2021/23/b.py
@@ -1,9 +1,420 @@
 import os
-import re
 import numpy as np
-import pandas as pd
-from tools import *
+from typing import List, Union, Dict, Tuple
+import numba
+from numba.experimental import jitclass
+import logging
+
+# numba_logger = logging.getLogger("numba")
+# numba_logger.setLevel(logging.DEBUG)
+
+
+# ROOM_ID_TO_DESIRED = list("ABCD")
+TYPE_TO_COST = {"A": 1, "B": 10, "C": 100, "D": 1_000}
+TYPE_TO_TYPE_ID = {"A": 0, "B": 1, "C": 2, "D": 3}
+# FORBIDDEN_HALLWAY_POS = np.array([2, 4, 6, 8])
+# HALLWAY_POS_TO_ROOM_ID = {2: 0, 4: 1, 6: 2, 8: 3}
+# ROOM_ID_TO_HALLWAY_POS = {v: k for k, v in HALLWAY_POS_TO_ROOM_ID.items()}
+# EMPTY_CHAR = -1
+# AMPHIPOD_ID_TO_TYPE = {}
+# AMPHIPOD_ID_TO_COST = {}
+
+spec = [
+    ("hallway", numba.types.int16[:]),
+    ("rooms", numba.types.int16[:, :]),
+    ("entered_room_amphipod_ids", numba.types.boolean[:]),
+    ("energy", numba.types.int16),
+    ("amphipod_id_to_type", numba.types.int16[:]),
+    ("amphipod_id_to_cost", numba.types.int16[:]),
+]
+
+
+@jitclass(spec)
+class Route:
+    def __init__(
+        self,
+        hallway,
+        rooms,
+        entered_room_amphipod_ids,
+        energy,
+        amphipod_id_to_type,
+        amphipod_id_to_cost,
+    ):
+        self.hallway = hallway
+        self.rooms = rooms
+        self.entered_room_amphipod_ids = entered_room_amphipod_ids
+        self.energy = energy
+        self.amphipod_id_to_type = amphipod_id_to_type
+        self.amphipod_id_to_cost = amphipod_id_to_cost
+
+    @staticmethod
+    def room_id_to_desired(room_id):
+        if room_id == 0:
+            return "A"
+        elif room_id == 1:
+            return "B"
+        elif room_id == 2:
+            return "C"
+        else:
+            return "D"
+
+    @staticmethod
+    def hallway_pos_to_room_id(hallway_pos):
+        if hallway_pos == 2:
+            return 0
+        elif hallway_pos == 4:
+            return 1
+        elif hallway_pos == 6:
+            return 2
+        else:
+            return 3
+
+    @staticmethod
+    def room_id_to_hallway_pos(room_id):
+        if room_id == 0:
+            return 2
+        elif room_id == 1:
+            return 4
+        elif room_id == 2:
+            return 6
+        else:
+            return 8
+
+    def is_complete(self):
+        if not np.all(self.hallway == -1):
+            return False
+        for i in range(len(self.rooms)):
+            for c in self.rooms[i]:
+                if self.amphipod_id_to_type[c] != i:
+                    return False
+        return True
+
+    def is_valid(self):
+        return True
+
+    def __lt__(self, other):
+        return self.energy < other.energy
+
+    def tag(self):
+        s = np.empty(
+            self.hallway.shape[0] + self.rooms.shape[0] * self.rooms.shape[1],
+            dtype=np.int16,
+        )
+        for i in range(len(self.hallway)):
+            s[i] = self.hallway[i]
+        for i in range(len(self.rooms)):
+            for j in range(len(self.rooms[i])):
+                s[self.hallway.shape[0] + i * self.rooms.shape[1] + j] = self.rooms[
+                    i, j
+                ]
+        return s
+
+    def all_possible_next_states(self):
+        occupied = set([i for i, c in enumerate(self.hallway) if c != -1])
+        outputs = []
+
+        # Hallway chaps can move to other hallway spots or into empty rooms (can't pass each other)
+        for hallway_pos in range(len(self.hallway)):
+
+            amphipod_id = self.hallway[hallway_pos]
+
+            if amphipod_id == -1:
+                continue
+
+            for new_pos in range(len(self.hallway)):
+
+                if new_pos in occupied:
+                    continue
+                elif new_pos < hallway_pos:
+                    crossing_other = False
+                    for o in occupied:
+                        if new_pos < o and o < hallway_pos:
+                            crossing_other = True
+                            break
+                else:
+                    crossing_other = False
+                    for o in occupied:
+                        if new_pos > o and o > hallway_pos:
+                            crossing_other = True
+                            break
+                if crossing_other:
+                    continue
+
+                if new_pos in np.array([2, 4, 6, 8], dtype=np.int16):
+                    # Go from hallway into a room
+
+                    room_id = self.hallway_pos_to_room_id(new_pos)
+                    if self.amphipod_id_to_type[amphipod_id] != self.room_id_to_desired(
+                        room_id
+                    ):
+                        continue
+                    room = self.rooms[room_id]
+
+                    if not np.any(room == -1):
+                        continue
+
+                    vacancy = -1
+                    for i in range(len(room)):
+                        if room[i] == -1:
+                            vacancy = i
+
+                    new_rooms = self.rooms.copy()
+                    new_rooms[room_id, vacancy] = amphipod_id
+
+                    new_hallway = self.hallway.copy()
+                    new_hallway[hallway_pos] = -1
+
+                    extra_energy = self.amphipod_id_to_cost[amphipod_id] * vacancy
+                    new_entered_ids = self.entered_room_amphipod_ids.copy()
+                    new_entered_ids[amphipod_id] = True
+
+                    outputs.append(
+                        Route(
+                            new_hallway,
+                            new_rooms,
+                            new_entered_ids,
+                            self.energy
+                            + np.abs(new_pos - hallway_pos)
+                            * self.amphipod_id_to_cost[amphipod_id]
+                            + self.amphipod_id_to_cost[amphipod_id]
+                            + extra_energy,
+                            self.amphipod_id_to_type,
+                            self.amphipod_id_to_cost,
+                        )
+                    )
+                else:
+                    # Go from one hallway position to another
+                    new_hallway = self.hallway.copy()
+                    new_hallway[hallway_pos] = -1
+                    new_hallway[new_pos] = amphipod_id
+
+                    new_rooms = self.rooms.copy()
+
+                    outputs.append(
+                        Route(
+                            new_hallway,
+                            new_rooms,
+                            self.entered_room_amphipod_ids.copy(),
+                            self.energy
+                            + self.amphipod_id_to_cost[amphipod_id]
+                            * np.abs(new_pos - hallway_pos),
+                            self.amphipod_id_to_type,
+                            self.amphipod_id_to_cost,
+                        )
+                    )
+
+        # Room chaps can move out only if the have not already done so
+        for room_id, room in enumerate(self.rooms):
+
+            next_amphipod_id = [(i, c) for i, c in enumerate(room) if c != -1]
+            if not len(next_amphipod_id):
+                continue
+            room_space = next_amphipod_id[0][0]
+            amphipod_id = next_amphipod_id[0][1]
+
+            if self.entered_room_amphipod_ids[amphipod_id]:
+                continue
+
+            for new_pos in range(len(self.hallway)):
+                room_pos = self.room_id_to_hallway_pos(room_id)
+
+                if new_pos in occupied:
+                    continue
+                elif new_pos in np.array([2, 4, 6, 8], dtype=np.int16):
+                    continue
+                elif new_pos < room_pos:
+                    crossing_other = False
+                    for o in occupied:
+                        if new_pos < o and o < room_pos:
+                            crossing_other = True
+                            break
+                else:
+                    crossing_other = False
+                    for o in occupied:
+                        if new_pos > o and o > room_pos:
+                            crossing_other = True
+                            break
+                if crossing_other:
+                    continue
+
+                new_rooms = self.rooms.copy()
+                new_rooms[room_id, room_space] = -1
+
+                extra_energy = self.amphipod_id_to_cost[amphipod_id] * room_space
+
+                new_hallway = self.hallway.copy()
+                new_hallway[new_pos] = amphipod_id
+
+                outputs.append(
+                    Route(
+                        new_hallway,
+                        new_rooms,
+                        self.entered_room_amphipod_ids.copy(),
+                        self.energy
+                        + np.abs(new_pos - room_pos)
+                        * self.amphipod_id_to_cost[amphipod_id]
+                        + self.amphipod_id_to_cost[amphipod_id]
+                        + extra_energy,
+                        self.amphipod_id_to_type,
+                        self.amphipod_id_to_cost,
+                    )
+                )
+        return outputs
+
+
+import heapq
+
+DEBUG = False
+
+
+class AStarException(Exception):
+    pass
+
+
+def a_star(
+    initial_state, tag_func=str, return_status=False, test_tag_at_best_option=False
+):
+    """Perform the A* search algorithm
+    The initial_state should be a subclass of State (below)
+    that implements:
+    - is_complete - boolean of whether this state is the desired result
+    - is_valid - boolean
+    - all_possible_next_states - iterable of states after this one
+
+    Arguments:
+        initial_state {user_class with above methods}
+
+    Keyword Arguments:
+        tag_func {callable} -- [function to tag each
+        state with so we can know if it has already been seen
+        ] (default: {str})
+
+        return_status {boolean} -- Rather than returning the
+        final state, return a dictionary summarising the search
+
+    Returns:
+        [user_class(State)] -- [Desired search result]
+    """
+
+    i = 0
+    possible_states = [(initial_state[0], i)]
+    data = {i: initial_state[1]}
+    seen = set()
+    n_tests = 0
+    is_complete = False
+
+    while len(possible_states):
+
+        _, best_option_key = heapq.heappop(possible_states)
+        best_option = data.pop(best_option_key)
+        n_tests += 1
+
+        if test_tag_at_best_option:
+            tag = tag_func(best_option)
+            if tag in seen:
+                if DEBUG:
+                    print(f"Skipping {tag} as already seen")
+                continue
+            seen.add(tag)
+
+        if best_option.energy == 40:
+            if best_option.hallway[3] == 2:
+                print(best_option.hallway)
+                print(best_option.rooms)
+                print(best_option.energy)
+        
+                print()
+        
+        
+        if not best_option.energy % 100:
+            print(best_option.energy)
+        if DEBUG:
+            print(
+                f"Test {n_tests}, n_options {len(possible_states)}, best_option: {tag_func(best_option)}"
+            )
+        if best_option.is_complete():
+            if DEBUG:
+                print("Search complete")
+            is_complete = True
+            break
+
+        for s in best_option.all_possible_next_states():
+            if not s.is_valid():
+                if DEBUG:
+                    print(f"Skipping {s} as not valid")
+                continue
+            if not test_tag_at_best_option:
+                tag = tag_func(s)
+                if tag in seen:
+                    if DEBUG:
+                        print(f"Skipping {tag} as already seen")
+                    continue
+                seen.add(tag)
+            if DEBUG:
+                print("Adding new state to heap")
+
+            i += 1
+            data[i] = s
+            key = (s.energy, i)
+            heapq.heappush(possible_states, key)
+
+    if return_status:
+        return {
+            "seen": seen,
+            "best_option": best_option,
+            "n_tests": n_tests,
+            "is_complete": is_complete,
+        }
+    elif is_complete:
+        return best_option
+    else:
+        raise AStarException("Search did not complete")
 
 
 def run(inputs):
-    pass
+    lines = inputs.split(os.linesep)
+    hallway = np.full(lines[1].count("."), -1, dtype=np.int16)
+
+    top_amphipods = lines[2].replace("#", "").strip()
+    top_middle = "DCBA"
+    bottom_middle = "DBAC"
+    bottom_amphipods = lines[3].replace("#", "").strip()
+
+    all_amphipods = [
+        j
+        for i in [
+            top_amphipods,
+            bottom_amphipods,
+        ]  # [top_amphipods, top_middle, bottom_middle, bottom_amphipods]
+        for j in i
+    ]
+
+    amphipod_id_to_type = np.empty(len(all_amphipods), dtype=np.int16)
+    amphipod_id_to_cost = np.empty_like(amphipod_id_to_type, dtype=np.int16)
+
+    for amphipod_id, amphipod_type in enumerate(all_amphipods):
+        amphipod_id_to_type[amphipod_id] = TYPE_TO_TYPE_ID[amphipod_type]
+        amphipod_id_to_cost[amphipod_id] = TYPE_TO_COST[amphipod_type]
+
+    rooms = np.arange(len(all_amphipods), dtype=np.int16).reshape(
+        -1, len(top_amphipods)
+    ).T
+
+    entered_room_amphioid_id = np.full_like(amphipod_id_to_cost, False, dtype=bool)
+
+    initial_state = Route(
+        hallway=hallway,
+        rooms=rooms,
+        entered_room_amphipod_ids=entered_room_amphioid_id,
+        energy=0,
+        amphipod_id_to_type=amphipod_id_to_type,
+        amphipod_id_to_cost=amphipod_id_to_cost,
+    )
+
+    best_route = a_star(
+        initial_state=(0, initial_state),
+        test_tag_at_best_option=True,
+        tag_func=lambda x: str(x.tag()),
+    )
+    print(best_route)
+    return best_route.energy
