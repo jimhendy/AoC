fd325a496ffe9946a8dddd129adcba18881644d8
jimhendy <jimhendy88@gmail.com>
2024-04-25 08:05:47 +0100

    Update deps

--- 2023/21/b.py
+++ 2023/21/b.py
@@ -1,29 +1,19 @@
-import numpy as np
+from loguru import logger
 import numba as nb
+import numpy as np
 
+STEPS = 26501365
 
-@profile
-def step(
-    locations: set[complex], rocks: set[complex], grid_height: int, grid_width: int
-) -> set[complex]:
-    new_locations = set()
-    for location in locations:
-        for direction in [complex(0, 1), complex(1, 0), complex(0, -1), complex(-1, 0)]:
-            new_location = location + direction
-            if new_location.real < 0 or new_location.real >= grid_width:
-                continue
-            if new_location.imag < 0 or new_location.imag >= grid_height:
-                continue
-            if new_location in rocks:
-                continue
-            new_locations.add(new_location)
-    return new_locations
-
+@nb.njit
+def _location_in_original_grid(location: complex, grid_width: int, grid_height: int) -> complex:
+    return complex(location.real % grid_width, location.imag % grid_height)
 
 def run(inputs: str) -> int:
-    rocks = set()
-    grid_width, grid_height = None, 0
-    starting_location = None
+    logger.info(f"Calculating for {STEPS = }")
+    rocks: set[complex] = set()
+    grid_width: int = 0
+    grid_height: int = 0
+    starting_location: complex = complex()
 
     for y, line in enumerate(inputs.splitlines()):
         characters = list(line)
@@ -35,11 +25,100 @@ def run(inputs: str) -> int:
             elif c == "S":
                 starting_location = complex(x, y)
         grid_height += 1
+        
+    logger.info(f"Starting location: {starting_location}")
+    logger.info(f"Grid width: {grid_width}, grid height: {grid_height}")
 
-    locations = set([starting_location])
-    for _ in range(26501365):
-        if _ % 10000 == 0:
-            print(_, len(locations))
-        locations = step(locations, rocks, grid_height, grid_width)
+    # Check if there are any rocks directly up, down, left or right of the starting position
+    for step in [1j, -1j, 1, -1]:
+        position = starting_location
+        while True:
+            position += step
+            if position in rocks:
+                logger.error(f"Found rock at {position}. There is not a clear path to the edge of the grid in the direction {step}")
+            if position.real < 0 or position.imag < 0:
+                break
+            if position.real == grid_width:
+                break
+            if position.imag == grid_height:
+                break
+            
 
-    return len(locations)
+            
+    # Find how many free spaces (".") are accessible from the starting position on even or odd steps
+    even_spaces = {starting_location}
+    odd_spaces = set()
+    step_num = 0
+    previous_spaces = even_spaces
+    
+    while previous_spaces:
+        step_num += 1
+        incumbent = odd_spaces if step_num % 2 else even_spaces
+        new_spaces = set()
+        for position in previous_spaces:
+            for step in [1j, -1j, 1, -1]:
+                new_position = position + step
+                if new_position.real < 0 or new_position.imag < 0:
+                    continue
+                if new_position.real == grid_width or new_position.imag == grid_height:
+                    continue
+                if new_position in rocks:
+                    continue
+                if new_position in incumbent:
+                    continue
+                incumbent.add(new_position)
+                new_spaces.add(new_position)
+                
+        previous_spaces = new_spaces
+        
+    logger.info(f"Even spaces: {len(even_spaces)}, odd spaces: {len(odd_spaces)}")
+    
+    # How many full tiles will be go up, down, left or right from the starting position in the STEPS steps
+    full_tiles = {}
+    remainders = {}
+    print(f"Starting location: {starting_location}")
+    for step in [1j, -1j, 1, -1]:
+        
+        # How many steps from the starting position to the edge of the original grid
+        position = starting_location
+        
+        grid_size = grid_width if step.real else grid_height
+        edge = 0 if (step.real + step.imag) < 0 else grid_size - 1
+        start_loc = starting_location.real if step.real else starting_location.imag
+        steps_to_edge = abs(edge - start_loc)
+        
+        print(f"Steps to edge: {steps_to_edge} for {step}")
+        
+        steps_after_edge = max(STEPS - steps_to_edge, 0)
+        full, remainder = divmod(steps_after_edge, grid_size)
+        
+        full_tiles[step] = int(full)
+        remainders[step] = int(remainder)       
+        
+        print(f"Full: {full}, remainder: {remainder}")
+        
+    max_width = full_tiles[1] + full_tiles[-1]
+    max_height = full_tiles[1j] + full_tiles[-1j]
+    
+    total_full = 0
+    width = 1
+    for _ in range(full_tiles[1]):
+        total_full += 2 * width
+        width += 2
+        
+    total_full += max_width
+    
+    # Add up the contributions fro the remainders
+    
+    
+    
+    if STEPS % 2:
+        print("Odd")
+        return len(odd_spaces) * total_full
+    else:
+        print("Even")
+        return len(even_spaces) * total_full
+    
+    print(f"Max width: {max_width}, max height: {max_height}")
+        
+    return 9
