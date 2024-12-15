712642f313882012a76a198e69aea7c324e3c929
jimhendy88 <jimhendy88@gmail.com>
2024-12-15 21:23:48 +0000

    2024 Day 15

---
+++ 2024/15/a.py
@@ -0,0 +1,70 @@
+from tools.inputs import parse_grid
+from typing import Literal
+from functools import lru_cache
+
+EMPTY = "."
+BOX = "O"
+WALL = "#"
+
+CACHE = {}
+
+
+def box_destination(
+    grid: dict[complex:str], box_location: complex, direction: complex
+) -> complex | Literal[False]:
+    """
+    The destination of the pushed box.
+
+    If multiple boxes are pushed, the destination is the next empty cell in the
+    direction of the push.
+
+    Boxes cannot be pushed- through walls.
+
+    If the box cannot be moved, return False.
+    """
+
+    cache_key = (str(grid), box_location, direction)
+    if cache_key in CACHE:
+        return CACHE[cache_key]
+
+    destination = box_location + direction
+    while grid[destination] == BOX:
+        destination += direction
+    if grid[destination] == WALL:
+        CACHE[cache_key] = False
+        return False
+    CACHE[cache_key] = destination
+    return destination
+
+
+def run(inputs: str) -> int:
+    grid, commands = inputs.split("\n\n")
+    commands = "".join(commands.split())
+    grid = parse_grid(grid)
+
+    robot = next(k for k, v in grid.items() if v == "@")
+    grid[robot] = EMPTY
+
+    for command in commands:
+        direction = {
+            "<": complex(-1, 0),
+            ">": complex(1, 0),
+            "^": complex(0, -1),
+            "v": complex(0, 1),
+        }[command]
+        next_pos = robot + direction
+        if grid[next_pos] == WALL:
+            continue
+
+        if grid[next_pos] == BOX:
+            box_dest = box_destination(grid, next_pos, direction)
+            if not box_dest:
+                continue
+            grid[next_pos] = EMPTY
+            grid[box_dest] = BOX
+
+        robot = next_pos
+
+    # Find the GPS of all boxes
+    boxes = [k for k, v in grid.items() if v == BOX]
+    return sum(100 * b.imag + b.real for b in boxes)
---
+++ 2024/15/b.py
@@ -0,0 +1,99 @@
+from tools.inputs import parse_grid
+from typing import Literal
+
+EMPTY = "."
+LEFT_BOX = "["
+RIGHT_BOX = "]"
+WALL = "#"
+
+
+def move_boxes(
+    grid: dict[complex:str], location: complex, direction: complex
+) -> list[complex] | Literal[False]:
+    """
+    Return a list of **original** box locations that can be moved.
+
+    If a box cannot be moved, return False.
+
+    If we are pushing left or right, things are pretty simple.
+    If moving up or down, we need also move the other half of any boxes we push.
+    """
+    if grid[location] == EMPTY:
+        return []
+    if grid[location] == WALL:
+        return False
+    if direction.real:
+        down_stream = move_boxes(grid, location + direction, direction)
+        if down_stream is False:
+            return False
+        return [location] + down_stream
+    else:
+        is_left = grid[location] == LEFT_BOX
+        other_loc = location + (1 if is_left else -1)
+        down_stream = move_boxes(grid, location + direction, direction)
+        if down_stream is False:
+            return False
+        down_stream_other = move_boxes(grid, other_loc + direction, direction)
+        if down_stream_other is False:
+            return False
+        return [location, other_loc] + down_stream + down_stream_other
+
+
+def widen_grid(grid: str) -> str:
+    """
+    If the tile is #, the new map contains ## instead.
+    If the tile is O, the new map contains [] instead.
+    If the tile is ., the new map contains .. instead.
+    If the tile is @, the new map contains @. instead.
+    """
+    return "\n".join(
+        "".join({"#": "##", "O": "[]", ".": "..", "@": "@."}.get(c, c) for c in line)
+        for line in grid.split("\n")
+    )
+
+
+def run(inputs: str) -> int:
+    grid, commands = inputs.split("\n\n")
+    grid = widen_grid(grid)
+    commands = "".join(commands.split())
+    grid = parse_grid(grid)
+
+    robot = next(k for k, v in grid.items() if v == "@")
+    grid[robot] = EMPTY
+
+    for command in commands:
+        direction = {
+            "<": complex(-1, 0),
+            ">": complex(1, 0),
+            "^": complex(0, -1),
+            "v": complex(0, 1),
+        }[command]
+        next_pos = robot + direction
+        if grid[next_pos] == WALL:
+            continue
+
+        if grid[next_pos] in (LEFT_BOX, RIGHT_BOX):
+            boxes = move_boxes(grid, next_pos, direction)
+            if boxes is False:
+                continue
+            boxes = set(boxes)
+
+            # Order of the shift matters, sort in -direction
+            if command == "<":
+                boxes = sorted(boxes, key=lambda b: b.real, reverse=False)
+            elif command == ">":
+                boxes = sorted(boxes, key=lambda b: b.real, reverse=True)
+            elif command == "^":
+                boxes = sorted(boxes, key=lambda b: b.imag, reverse=False)
+            elif command == "v":
+                boxes = sorted(boxes, key=lambda b: b.imag, reverse=True)
+
+            for box in boxes:
+                grid[box + direction] = grid[box]
+                grid[box] = EMPTY
+
+        robot = next_pos
+
+    # Find the GPS of all boxes
+    boxes = [k for k, v in grid.items() if v == LEFT_BOX]
+    return sum(100 * b.imag + b.real for b in boxes)
