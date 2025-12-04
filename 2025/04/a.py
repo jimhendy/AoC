5f0e631dd90d2f5b87b019d687971ed0c8c67f5d
jimhendy <jimhendy@hotmail.com>
2025-12-04 08:49:07 +0000

    2025 day 4

--- 2025/04/a.py
+++ 2025/04/a.py
@@ -1,2 +1,9 @@
+from tools.inputs import parse_grid, nb8
+
 def run(input: str) -> int:
-    return 0
+    points = set(parse_grid(input, whitelist="@").keys())
+    total = 0
+    for p in points:
+        if len(points.intersection(nb8(p))) < 4:
+            total += 1
+    return total
--- 2025/04/b.py
+++ 2025/04/b.py
@@ -1,2 +1,15 @@
+from tools.inputs import parse_grid, nb8
+
 def run(input: str) -> int:
-    return 0
+    points = set(parse_grid(input, whitelist="@").keys())
+    total = 0
+    while True:
+        to_remove = set()
+        for p in points:
+            if len(points.intersection(nb8(p))) < 4:
+                total += 1
+                to_remove.add(p)
+        if not to_remove:
+            break
+        points.difference_update(to_remove)
+    return total
