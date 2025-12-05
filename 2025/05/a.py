095986cf2d08b899c1c65207d7cd71728489b5e8
jimhendy <jimhendy88@gmail.com>
2025-12-05 07:10:33 +0000

    Implement range processing logic in run function for both a.py and b.py

--- 2025/05/a.py
+++ 2025/05/a.py
@@ -1,2 +1,12 @@
 def run(input: str) -> int:
-    return 0
+    ranges, ids = input.strip().split("\n\n")
+    ranges = [list(map(int, line.split("-"))) for line in ranges.splitlines()]
+    ids = list(map(int, ids.splitlines()))
+
+    total = 0
+    for id in ids:
+        for r in ranges:
+            if r[0] <= id <= r[1]:
+                total += 1
+                break
+    return total
--- 2025/05/b.py
+++ 2025/05/b.py
@@ -1,2 +1,43 @@
+from dataclasses import dataclass
+
+@dataclass
+class Range:
+    start: int
+    end: int
+
+    def can_combine(self, other: "Range") -> bool:
+        return not (self.end < other.start - 1 or other.end < self.start - 1)
+
+
 def run(input: str) -> int:
-    return 0
+    ranges_, _ = input.strip().split("\n\n")
+    ranges = []
+    for line in ranges_.splitlines():
+        start, end = map(int, line.split("-"))
+        ranges.append(Range(start, end))
+
+    change = True
+    while change:
+        change = False
+        new_ranges = []
+        used = [False] * len(ranges)
+
+        for i, current in enumerate(ranges):
+            if used[i]:
+                continue
+            for j, other in enumerate(ranges[i + 1:], start=i + 1):
+                if used[j]:
+                    continue
+                if current.can_combine(other):
+                    current = Range(start=min(current.start, other.start), end=max(current.end, other.end))
+                    used[j] = True
+                    change = True
+            new_ranges.append(current)
+            used[i] = True
+
+        ranges = new_ranges
+
+    total = 0
+    for r in ranges:
+        total += r.end - r.start + 1
+    return total
